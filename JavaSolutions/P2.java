753b2b6b4b23b5d2d45860fe160fd976689811ea
sam-pawar <86359037+sam-pawar@users.noreply.github.com>
2025-03-12 18:10:56 +0530

    Create P2.java

---
+++ JavaSolutions/P2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;        
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0){
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int rel = sum % 10;
+            carry = sum / 10;
+
+            ListNode result = new ListNode(rel);
+            tail.next = result;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        return (dummyHead.next != null) ? dummyHead.next : null ;
+
+    }
+}
